03a0343ed6d1da4044abc3f4cd256272250329d4
emjeisomDev <emjeisom.dev@gmail.com>
2025-07-05 14:25:35 -0300

    Teste Envio

---
+++ CW_CSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿//-Transforme o trecho abaixo em forma de documentação em C#
+//-Faça uma documentação no idioma original.
+//-Faça uma documentaçã em português pt-br. 
+//-Caso o idioma original seja pt-br faça apenas em pt-br.
+//-NÃO FAÇA NENHUM CÓDIGO. APENAS A DOCUMENTAÇÃO.
+//-NÃO INTERPRETE O QUE DEVE SER FEITO. APENAS TRANSFORME O TEXTO NO FORMATO DE DOCUMENTAÇÃO DE CÓDIGO
+//-Dê um nome intuitivo para a classe
+// ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+// ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+//Otimize o código C# abaixo utilizando o mínimo de linhas de código possível
+//Dê preferência ao LINQ e funções LAMBDA
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
